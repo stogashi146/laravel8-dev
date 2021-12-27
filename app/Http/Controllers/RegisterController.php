6991ba700725940bdeb16c191dc8dd413f33f65c
stogashi146 <stogashi146@gmail.com>
2021-12-27 13:59:13 +0900

    [add]ユーザー登録処理

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,8 +15,14 @@
     public function store(Request $request){
       $request->validate([
         "name"=>"required|string|max:255",
-        "email"=>$request->email,
-        "password"=>Hash::make($request -> password),
+        "email"=>"required|string|email|max:255|unique:users",
+        "password"=>"required|string|confirmed|min:8",
+      ]);
+
+      $user = User::create([
+        "name" => $request->name,
+        "email" => $request->email,
+        "password" => Hash::make($request->password),
       ]);
 
       return view("regist.complete", compact("user"));
--- resources/views/regist/register.blade.php
+++ resources/views/regist/register.blade.php
@@ -7,7 +7,7 @@
   <title>ユーザー登録フォーム</title>
 </head>
 <body>
-  <form action="registform" action="/register" method="post" id="registform">
+  <form action="/register" method="post" id="registform">
     {{ csrf_field() }}
     <dl>
       <dt>名前：</dt>
@@ -37,6 +37,7 @@
         <span>{{ $errors->first("password_confirmation") }}</span>
       </dd>
     </dl>
+    <button type="submit" name="action" value="send">送信</button>
   </form>
 </body>
 </html>
